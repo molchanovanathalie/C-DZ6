d43ece793c8b6253d58d08d524df14ebdf88e9b7
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-11-02 21:11:15 +0300

    add 003

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,7 +1,10 @@
 ﻿//Написать программу масштабирования фигуры
 // точка начала координат находится в точке пересечения диагоналей прямоугольника
 
-string Scaling (int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int k);
+// (0,0) (2,0) (2,2) (0,2) 
+
+string Scaling(double x1, double y1, double x2, double y2, 
+double x3, double y3, double x4, double y4, double k)
 {
     x1 = x1 * k;
     y1 = y1 * k;
@@ -11,8 +14,6 @@ string Scaling (int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, 
     y3 = y3 * k;
     x4 = x4 * k;
     y4 = y4 * k;
-    return $"(({x1},{y1}),({x2}, {y2}),({x3}, {y3}),({x4}, {y4}))";
-   
+    return $"({x1}, {y1}) ({x2}, {y2}) ({x3}, {y3}) ({x4}, {y4})";
 }
-Console.Write(Scaling(-1, -1, -1, 1, 1, 1 ,1, -1, 1));
-//в чем ошибка? не работает...
+Console.WriteLine(Scaling(0, 0, 2, 0, 2, 2, 0, 2, 2));
