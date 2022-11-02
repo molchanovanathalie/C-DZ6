8f98d97a1ef7d6a7893754b9765fda62162a245c
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-11-02 21:36:50 +0300

    add 004

---
+++ 004.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу копирования массива
+int[] array = new int[7];
+int arrayCop;
+
+Console.Write("Original ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 30);
+    Console.Write(array[i]);
+}
+Console.WriteLine();
+Console.Write("CopyPast ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCop = array[i];
+    Console.Write(array[i]);
+}
